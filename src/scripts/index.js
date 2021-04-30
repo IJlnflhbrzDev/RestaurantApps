62b4931daad8f2b5031a65c4a2ffb22fc58fb40b
unknown <ijlnflhbrzdev@gmail.com>
2021-05-01 02:00:49 +0700

    menyelesaikan fix style => navbar,hero & menerapkan skip to content di file __aksesibilitas.scss & menerapkan fungsionalitas tabindex pada element seperti button dan tautan atau href

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,21 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+const btn_nav_mobile = document.getElementById("nav-drawwer");
+const handle_drawwer = document.querySelector(".header");
+const nav_DrawwerMobile = document.getElementById('nav-drawwer_mobile');
+
+btn_nav_mobile.addEventListener("click", function () {
+     nav_DrawwerMobile.classList.toggle("show-menu-mobile");
+})
+
+handle_drawwer.addEventListener("click", function () {
+     nav_DrawwerMobile.classList.remove('show-menu-mobile')
+})
+
+
+alert(`
+
+PR = Menampilkan Daftar Restoran
+`)
+
