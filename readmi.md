45c0fc245cbb86055e72f46b8e8f750bc56695ab
unknown <ijlnflhbrzdev@gmail.com>
2021-05-01 20:50:41 +0700

    Final Project! telah menampilkan data dan sudah tahap production tingal tungu review dari team dicoding

--- readmi.md
+++
@@ -1,3 +0,0 @@
-Restaurant-Apps
-
-project ini adalah submission perjalanan awal front end developer expert dari dicoding
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
+import Data from "../DATA.json";
+
 const btn_nav_mobile = document.getElementById("nav-drawwer");
 const handle_drawwer = document.querySelector(".header");
 const nav_DrawwerMobile = document.getElementById('nav-drawwer_mobile');
@@ -13,9 +15,27 @@ handle_drawwer.addEventListener("click", function () {
      nav_DrawwerMobile.classList.remove('show-menu-mobile')
 })
 
+// LOOPONG DATA & menampilkan data
+function CardHtml(resto) {
+     return `
+     <article>
+                    <div class="card" tabindex="0">
+                         <img src="${resto.pictureId}"  class="card-img" alt=${resto.name} tabindex="0">
+                         <div class="card-lokasi">
+                              <h3 tabindex="0">Kota,${resto.city}</h3>
+                         </div>
+                         <div class="card-body">
+                              <h4 class="card-rating" tabindex="0">Rating ${resto.rating}</h4>
+                              <h5 class="card-title" tabindex="0">${resto.name}</h5>
+                              <p class="card-text" tabindex="0">${resto.description}</p>
+                         </div>
+                    </div>
+     </article>
 
-alert(`
+     `
+}
+Data.restaurants.forEach(items => {
+     const conten_main = document.querySelector(".conten");
+     conten_main.innerHTML += CardHtml(items);
 
-PR = Menampilkan Daftar Restoran
-`)
-
+})
